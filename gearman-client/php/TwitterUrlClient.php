f6717e3e4257223ccf4ded6feedd2a7ba8620a71
Marcus <marcus#exp2013@t3sec.info>
2013-09-04 21:55:37 +0200

    [BUGFIX] Fix broken test for existing records which resulted in multiple entries for hosts w/o subdomain

--- gearman-client/php/TwitterUrlClient.php
+++ gearman-client/php/TwitterUrlClient.php
@@ -38,13 +38,13 @@ unset($gearmanStatus);
 $isSuccessful = TRUE;
 
 
-$client= new GearmanClient();
+$client = new GearmanClient();
 $client->addServer($gearmanHost, $gearmanPort);
 
 $selectQuery = 'SELECT t.tweet_id,t.created,t.tweet_processed,u.url_text '
-			.  'FROM twitter_tweet t JOIN twitter_url u ON (t.tweet_id = u.fk_tweet_id) '
-			.  'WHERE NOT t.tweet_processed '
-			.  'ORDER BY t.created ASC;';
+			 . 'FROM twitter_tweet t JOIN twitter_url u ON (t.tweet_id = u.fk_tweet_id) '
+			 . 'WHERE NOT t.tweet_processed '
+			 . 'ORDER BY t.created ASC;';
 $res = $mysqli->query($selectQuery);
 #fwrite(STDOUT, sprintf('DEBUG: Query: %s' . PHP_EOL, $selectQuery));
 if (is_object($res)) {
@@ -56,22 +56,23 @@ if (is_object($res)) {
 
 		if (!isShortenerServiceHost($objUrl->get('host'))) {
 			$selectQuery = sprintf('SELECT 1 '
-						.  'FROM host '
-						.  'WHERE created IS NOT NULL AND host_scheme LIKE \'%s\' AND host_subdomain LIKE %s AND host_domain LIKE \'%s\' '
-						.  'LIMIT 1;',
+				. 'FROM host '
+				. 'WHERE created IS NOT NULL AND host_scheme LIKE \'%s\' AND host_subdomain %s AND host_domain LIKE \'%s\' '
+				. 'LIMIT 1;',
 				$objUrl->get('scheme'),
-				(is_null($objUrl->get('subdomain')) ? 'NULL' : '\'' . mysqli_real_escape_string($mysqli, $objUrl->get('subdomain')) . '\''),
+				(is_null($objUrl->get('subdomain')) ? 'IS NULL' : 'LIKE \'' . mysqli_real_escape_string($mysqli, $objUrl->get('subdomain')) . '\''),
 				$objUrl->get('registerableDomain')
 			);
 			$selectRes = $mysqli->query($selectQuery);
 			#fwrite(STDOUT, sprintf('DEBUG: Query: %s' . PHP_EOL, $selectQuery));
-			if (is_object($selectRes))  {
+
+			if (is_object($selectRes)) {
 				if ($selectRes->num_rows > 0) {
 					echo('PASS' . PHP_EOL);
 					$updateQuery = sprintf('UPDATE twitter_tweet SET tweet_processed = 1 WHERE tweet_id=%u;',
 						$row['tweet_id']
 					);
-					$updateResult= $mysqli->query($updateQuery);
+					$updateResult = $mysqli->query($updateQuery);
 					#fwrite(STDOUT, sprintf('DEBUG: Query: %s' . PHP_EOL, $updateQuery));
 					if (!is_bool($updateResult) || !$updateResult) {
 						fwrite(STDERR, sprintf('ERROR: %s (Errno: %u)' . PHP_EOL, $mysqli->error, $mysqli->errno));
@@ -96,7 +97,7 @@ if (is_object($res)) {
 		#print_r($detectionResult);
 
 		if (is_object($detectionResult)) {
-			if (is_null($detectionResult->port) || is_null($detectionResult->ip))  continue;
+			if (is_null($detectionResult->port) || is_null($detectionResult->ip)) continue;
 
 			$portId = getPortId($mysqli, $detectionResult->port);
 
@@ -166,11 +167,11 @@ function isShortenerServiceHost($host) {
 function persistHost($objMysql, $serverId, $host) {
 
 	$selectQuery = sprintf('SELECT host_id '
-						.  'FROM host '
-						.  'WHERE fk_server_id=%u AND host_scheme LIKE \'%s\' AND host_subdomain LIKE %s AND host_domain LIKE \'%s\';',
+		. 'FROM host '
+		. 'WHERE fk_server_id=%u AND host_scheme LIKE \'%s\' AND host_subdomain %s AND host_domain LIKE \'%s\';',
 		$serverId,
 		$host->scheme,
-		(is_null($host->subdomain) ? 'NULL' : '\'' . mysqli_real_escape_string($objMysql, $host->subdomain) . '\''),
+		(is_null($host->subdomain) ? 'IS NULL' : 'LIKE \'' . mysqli_real_escape_string($objMysql, $host->subdomain) . '\''),
 		$host->registerableDomain
 	);
 	$selectRes = $objMysql->query($selectQuery);
@@ -183,12 +184,12 @@ function persistHost($objMysql, $serverId, $host) {
 			$insertQuery = sprintf('INSERT INTO host(typo3_installed,typo3_versionstring,host_name,host_scheme,host_subdomain,host_domain,host_suffix,host_path,created,fk_server_id) ' .
 				'VALUES(%u,%s,NULL,\'%s\',%s,\'%s\',%s,%s,\'%s\',%u);',
 				($host->TYPO3 ? 1 : 0),
-				($host->TYPO3 && !empty($host->TYPO3version) ? '\'' . mysqli_real_escape_string($objMysql, $host->TYPO3version)  . '\'' : 'NULL'),
+				($host->TYPO3 && !empty($host->TYPO3version) ? '\'' . mysqli_real_escape_string($objMysql, $host->TYPO3version) . '\'' : 'NULL'),
 				mysqli_real_escape_string($objMysql, $host->scheme),
-				(is_null($host->subdomain) ? 'NULL' : '\'' . mysqli_real_escape_string($objMysql,$host->subdomain) . '\''),
+				(is_null($host->subdomain) ? 'NULL' : '\'' . mysqli_real_escape_string($objMysql, $host->subdomain) . '\''),
 				$host->registerableDomain,
-				(is_null($host->publicSuffix) ? 'NULL' : '\'' . mysqli_real_escape_string($objMysql,$host->publicSuffix) . '\''),
-				(is_null($host->path) ? 'NULL' : '\'' . mysqli_real_escape_string($objMysql,$host->path) . '\''),
+				(is_null($host->publicSuffix) ? 'NULL' : '\'' . mysqli_real_escape_string($objMysql, $host->publicSuffix) . '\''),
+				(is_null($host->path) ? 'NULL' : '\'' . mysqli_real_escape_string($objMysql, $host->path) . '\''),
 				$date->format('Y-m-d H:i:s'),
 				$serverId
 			);
@@ -202,19 +203,19 @@ function persistHost($objMysql, $serverId, $host) {
 			$row = $selectRes->fetch_assoc();
 
 			$updateQuery = sprintf('UPDATE host '
-								.  'SET host_scheme=%s,host_subdomain=%s,host_domain=%s,host_suffix=%s,host_path=%s,typo3_installed=%u,typo3_versionstring=%s,updated=\'%s\',host_name=NULL '
-								.  'WHERE host_id=%u;',
+				. 'SET host_scheme=%s,host_subdomain=%s,host_domain=%s,host_suffix=%s,host_path=%s,typo3_installed=%u,typo3_versionstring=%s,updated=\'%s\',host_name=NULL '
+				. 'WHERE host_id=%u;',
 				(is_null($host->scheme) ? NULL : '\'' . mysqli_real_escape_string($objMysql, $host->scheme) . '\''),
 				(is_null($host->subdomain) ? 'NULL' : '\'' . mysqli_real_escape_string($objMysql, $host->subdomain) . '\''),
 				(is_null($host->registerableDomain) ? NULL : '\'' . mysqli_real_escape_string($objMysql, $host->registerableDomain) . '\''),
 				(is_null($host->publicSuffix) ? NULL : '\'' . mysqli_real_escape_string($objMysql, $host->publicSuffix) . '\''),
-				(is_null($host->path) ? 'NULL' : '\'' . mysqli_real_escape_string($objMysql,$host->path) . '\''),
+				(is_null($host->path) ? 'NULL' : '\'' . mysqli_real_escape_string($objMysql, $host->path) . '\''),
 				($host->TYPO3 ? 1 : 0),
-				($host->TYPO3 && !empty($host->TYPO3version) ? '\'' . mysqli_real_escape_string($objMysql, $host->TYPO3version)  . '\'' : 'NULL'),
+				($host->TYPO3 && !empty($host->TYPO3version) ? '\'' . mysqli_real_escape_string($objMysql, $host->TYPO3version) . '\'' : 'NULL'),
 				$date->format('Y-m-d H:i:s'),
 				$row['host_id']);
 			#fwrite(STDOUT, sprintf('DEBUG: Query: %s' . PHP_EOL, $updateQuery));
-			$updateResult= $objMysql->query($updateQuery);
+			$updateResult = $objMysql->query($updateQuery);
 			if (!is_bool($updateResult) || !$updateResult) {
 				fwrite(STDERR, sprintf('ERROR: %s (Errno: %u)' . PHP_EOL, $objMysql->error, $objMysql->errno));
 				return;
@@ -232,7 +233,7 @@ function persistServerPortMapping($mysqli, $serverId, $portId) {
 
 		if ($result->num_rows == 0) {
 			$foo = $mysqli->query("INSERT INTO server_port(fk_port_id,fk_server_id) VALUES (" . intval($portId) . ", " . intval($serverId) . ")");
-			if (!$foo)  echo "error-3: (" . $mysqli->errno . ") " . $mysqli->error;
+			if (!$foo) echo "error-3: (" . $mysqli->errno . ") " . $mysqli->error;
 		}
 
 		/* free result set */
@@ -243,12 +244,12 @@ function persistServerPortMapping($mysqli, $serverId, $portId) {
 function getServerId($mysqli, $server) {
 	$serverId = NULL;
 	/* Select queries return a resultset */
-	if ($result = $mysqli->query("SELECT server_id FROM server WHERE server_ip = INET_ATON('" . mysqli_real_escape_string($mysqli, $server) . "');" )) {
+	if ($result = $mysqli->query("SELECT server_id FROM server WHERE server_ip = INET_ATON('" . mysqli_real_escape_string($mysqli, $server) . "');")) {
 
 		if ($result->num_rows == 0) {
 			$date = new DateTime();
 			$foo = $mysqli->query("INSERT INTO server(server_ip,created) VALUES (INET_ATON('" . mysqli_real_escape_string($mysqli, $server) . "'), '" . $date->format('Y-m-d H:i:s') . "')");
-			if (!$foo)  echo "error-2: (" . $mysqli->errno . ") " . $mysqli->error;
+			if (!$foo) echo "error-2: (" . $mysqli->errno . ") " . $mysqli->error;
 			$serverId = $mysqli->insert_id;
 		} else {
 			$row = $result->fetch_assoc();
@@ -269,7 +270,7 @@ function getPortId($mysqli, $port) {
 
 		if ($result->num_rows == 0) {
 			$foo = $mysqli->query("INSERT INTO port(port_number) VALUES (" . intval($port) . ")");
-			if (!$foo)  echo "error-1: (" . $mysqli->errno . ") " . $mysqli->error;
+			if (!$foo) echo "error-1: (" . $mysqli->errno . ") " . $mysqli->error;
 			$portId = $mysqli->insert_id;
 		} else {
 			$row = $result->fetch_assoc();
